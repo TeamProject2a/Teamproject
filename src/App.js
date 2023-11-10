7525eb455a1b078c73e6a99300bb86d8407ef219
승엽 <iseung-yeob@iseung-yeob-ui-noteubug.local>
2023-11-10 18:39:55 +0900

    dustmq

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import React, { useState, useEffect} from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import '@css/style.css';
-import BoardList from '@components/BoardList';
-import BoardNew from '@components/BoardNew';
-import BoardContent from '@components/BoardContent';
-import Footer from '@components/Footer';
-import axios from 'axios';
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // mysql 연동 과정 
 
@@ -19,24 +12,16 @@ import ComImage from './com.png';
 // import 이미지 들 
 
 function App() {
-    useEffect(()) => {
-        axiois.get('/api/test')
-        .then(res => console.log(res))
-        .catch()
-
-    }
-    
-    const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-between', // 수직 방향 중앙 정렬
-    height: '100vh',
-    flexGrow: 1, // 컨텐츠가 더 커져도 화면 크기만큼 컨텐츠 영역을 확장
-
-  };
- 
-
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    fetch("/hello")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(data);
+      });
+  }, []);
 
   const contentWrapperStyle = {
     display: 'flex',
@@ -46,21 +31,20 @@ function App() {
     width: '100%',
     height: 'auto',
     background: '#F7D358',
-    padding : '40px 0 120px 0'
+    padding: '40px 0 120px 0',
   };
- 
 
   const centerWhiteStyle = {
     width: '80%',
     height: 'auto',
     background: 'white',
     display: 'flex',
-    margin: '0 auto', // 가운데 정렬을 위해 margin 추가
+    margin: '0 auto',
     justifyContent: 'space-between',
     flexDirection: 'column',
     padding: '20px',
     borderRadius: '10px',
-    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', // 테두리 추가
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
   };
 
   const homeWrapperStyle = {
@@ -73,7 +57,7 @@ function App() {
     width: '45px',
     height: '45px',
   };
-  
+
   const homeTextStyle = {
     fontSize: '22px',
     fontWeight: 'bold',
@@ -85,7 +69,7 @@ function App() {
     width: '35px',
     height: '35px',
     margin: '7px',
-    padding: '5px 20px 20px 20px'
+    padding: '5px 20px 20px 20px',
   };
 
   const tabtext = {
@@ -106,8 +90,7 @@ function App() {
     position: 'fixed',
     bottom: '0',
     zIndex: '1',
-    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', // 테두리 추가
-
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
   };
 
   const appBackgroundStyle = {
@@ -123,9 +106,8 @@ function App() {
     borderRadius: '10px',
     zIndex: '0',
     position: 'relative',
-    margin:'10px' // 변경된 위치 속성
+    margin: '10px',
   };
-
 
   const handleHomeImageClick = () => {
     window.location.href = '/App.js';
@@ -147,20 +129,22 @@ function App() {
     window.location.href = '/com.js';
   };
 
-  
   return (
-    <div className="App" style={containerStyle}>
-        <Router>
-            <div>
-                <Switch>
-                    <Route path='/' component={BoardList} exact/>
-                    <Route path='/BoardNew' component ={BoardNew} exact/>
-                    <Route path ='/BoardContent' component={BoardContent} exact />
-                </Switch>
-            </div>
-        </Router>
-         
+    <div className="App">
+      <ul>
+    {Array.isArray(message) ? (
+    message.map((v, idx) => <li key={`${idx}-${v}`}>{v}</li>)
+  ) : (
+    <li>서버연동 성공 </li>
+  )}
+</ul>
 
+      
+      <Router>
+        <div>
+          {/* Routes and Route removed since they are not used */}
+        </div>
+      </Router>
 
       <div style={contentWrapperStyle}>
         <div style={centerWhiteStyle}>
@@ -168,22 +152,23 @@ function App() {
             <img
               src={HomeImage}
               alt="home"
-              style={{  ...homeImageStyle }}
+              style={{ ...homeImageStyle }}
               onClick={handleHomeImageClick}
             />
             <span style={homeTextStyle}>오늘의 소식</span>
           </div>
 
           <div style={{ justifyContent: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={appBackgroundStyle}><p>내용 1</p></div>
-      <div style={appBackgroundStyle}><p>내용 2</p></div>
-      <div style={appBackgroundStyle}><p>내용 3</p></div>
-      <div style={appBackgroundStyle}><p>내용 4</p></div>
-      <div style={appBackgroundStyle}><p>내용 5</p></div>
-      <div style={appBackgroundStyle}><p>내용 6</p></div>
-      <div style={appBackgroundStyle}><p>내용 7</p></div>
-      <div style={appBackgroundStyle}><p>내용 8</p></div></div>
-      </div>
+            <div style={appBackgroundStyle}><p>내용 1</p></div>
+            <div style={appBackgroundStyle}><p>내용 2</p></div>
+            <div style={appBackgroundStyle}><p>내용 3</p></div>
+            <div style={appBackgroundStyle}><p>내용 4</p></div>
+            <div style={appBackgroundStyle}><p>내용 5</p></div>
+            <div style={appBackgroundStyle}><p>내용 6</p></div>
+            <div style={appBackgroundStyle}><p>내용 7</p></div>
+            <div style={appBackgroundStyle}><p>내용 8</p></div>
+          </div>
+        </div>
       </div>
       <div style={bottomTabStyle}>
         <div>
