5ec1726fae272bf5cbbd16bad15ea82f338fc547
승엽 <iseung-yeob@iseung-yeob-ui-noteubug.local>
2023-11-13 14:04:59 +0900

    스프링

--- demo/src/main/java/com/example/Controller/ControllerApplication.java
+++ demo/src/main/java/com/example/Controller/ControllerApplication.java
@@ -1,2 +1,22 @@
-package com.example.Controller;public class ControllerApplication {
+package com.example.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@SpringBootApplication
+public class ControllerApplication {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello from Spring!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
 }
